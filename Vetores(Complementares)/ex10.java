b176724b16ecb21175ed89136a0530a8261fbe5f
Joao2007Pedro <147161269+Joao2007Pedro@users.noreply.github.com>
2023-11-16 21:16:21 -0300

    Exercicio 10
    Ler um vetor A com dez elementos inteiros. Criar um vetor B que tenha os
    10 elementos de A em ordem crescente.

--- Vetores(Complementares)/ex10.java
+++ Vetores(Complementares)/ex10.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class ex10 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        final int TAM = 5;
+        final int TAM = 10;
         int[] a = new int[TAM];
         int[] b = new int[TAM];
         int temp;
